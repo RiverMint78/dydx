445c7566962591d1ed147b8feb3c78a31e12ec6a
RiverMint78 <67481978@qq.com>
2026-03-05 14:58:32 +0800

    simple pretty, reduce parentheses use Prec

--- src/Dydx/Pretty.hs
+++ src/Dydx/Pretty.hs
@@ -2,15 +2,30 @@ module Dydx.Pretty (pretty) where
 
 import Dydx.Expr
 
+-- pretty
 pretty :: (Show a) => Expr a -> String
-pretty (Const c) = show c
-pretty (Var v) = v
--- arity == 1
-pretty (Neg e) = "-(" ++ pretty e ++ ")"
-pretty (Sin e) = "sin(" ++ pretty e ++ ")"
-pretty (Cos e) = "cos(" ++ pretty e ++ ")"
-pretty (Exp e) = "exp(" ++ pretty e ++ ")"
--- arity == 2
-pretty (Add e1 e2) = "(" ++ pretty e1 ++ " + " ++ pretty e2 ++ ")"
-pretty (Sub e1 e2) = "(" ++ pretty e1 ++ " - " ++ pretty e2 ++ ")"
-pretty (Mul e1 e2) = "(" ++ pretty e1 ++ " * " ++ pretty e2 ++ ")"
+pretty = prettyPrec 0
+
+-- showParens: 是否外加括号
+showParens :: Bool -> String -> String
+showParens True s = "(" ++ s ++ ")"
+showParens False s = s
+
+prettyPrec :: (Show a) => Int -> Expr a -> String
+-- Leaf, no parentheses
+prettyPrec _ (Const c) = show c
+prettyPrec _ (Var v) = v
+-- Functions / Neg
+prettyPrec _ (Neg e) = "-(" ++ prettyPrec 0 e ++ ")"
+prettyPrec _ (Sin e) = "sin(" ++ prettyPrec 0 e ++ ")"
+prettyPrec _ (Cos e) = "cos(" ++ prettyPrec 0 e ++ ")"
+prettyPrec _ (Exp e) = "exp(" ++ prettyPrec 0 e ++ ")"
+-- Add / Sub
+prettyPrec p (Add e1 e2) =
+    showParens (p > 1) $ prettyPrec 1 e1 ++ " + " ++ prettyPrec 1 e2
+prettyPrec p (Sub e1 e2) =
+    -- x - (y - z)
+    showParens (p > 1) $ prettyPrec 1 e1 ++ " - " ++ prettyPrec 2 e2
+-- Mul
+prettyPrec p (Mul e1 e2) =
+    showParens (p > 2) $ prettyPrec 2 e1 ++ " * " ++ prettyPrec 2 e2
