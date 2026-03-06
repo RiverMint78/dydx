cbf850a9fe667b4d0e6c465b5e8f55d99e741deb
RiverMint78 <67481978@qq.com>
2026-03-06 16:42:10 +0800

    fix nested minus

--- src/Dydx/Pretty.hs
+++ src/Dydx/Pretty.hs
@@ -9,16 +9,22 @@ showParens :: Bool -> String -> String
 showParens True s = "(" ++ s ++ ")"
 showParens False s = s
 
+stripNeg :: (Num a, Ord a) => Expr a -> Maybe (Expr a)
+stripNeg (Const c) | c < 0 = Just (Const (negate c))
+stripNeg (Mul (Const c) e)
+    | c == -1 = Just e
+    | c < 0 = Just (Mul (Const (negate c)) e)
+stripNeg (Mul le re)
+    | Just le' <- stripNeg le = Just (Mul le' re)
+stripNeg _ = Nothing
+
 -- 0: OUTER
 -- 1: Add / Sub
--- 2: Mul / Div
--- 3: Unary Minus
+-- 2: Mul
+-- 2: Unary Minus
 -- 4: Pow
 prettyPrec :: (Show a, Ord a, Num a) => Int -> Expr a -> String
 prettyPrec _ NaN = "NaN"
--- Leaf
-prettyPrec _ (Const c) = show c
-prettyPrec _ (Var v) = v
 -- Functions
 prettyPrec _ (Sin e) = "sin(" ++ prettyPrec 0 e ++ ")"
 prettyPrec _ (Cos e) = "cos(" ++ prettyPrec 0 e ++ ")"
@@ -34,20 +40,21 @@ prettyPrec _ (Asinh e) = "asinh(" ++ prettyPrec 0 e ++ ")"
 prettyPrec _ (Acosh e) = "acosh(" ++ prettyPrec 0 e ++ ")"
 prettyPrec _ (Atanh e) = "atanh(" ++ prettyPrec 0 e ++ ")"
 -- Sub / Add
-prettyPrec p (Add le (Mul (Const c) re))
-    | c == -1 = showParens (p > 1) $ prettyPrec 1 le ++ " - " ++ prettyPrec 2 re
-    | c < 0 = showParens (p > 1) $ prettyPrec 1 le ++ " - " ++ prettyPrec 2 (Mul (Const (negate c)) re)
 prettyPrec p (Add le re) =
-    showParens (p > 1) $ prettyPrec 1 le ++ " + " ++ prettyPrec 1 re
-prettyPrec p (Mul (Const 1) e) = prettyPrec p e
-prettyPrec p (Mul e (Const 1)) = prettyPrec p e
--- Div / Mul
-prettyPrec p (Mul le (Pow re (Const (-1)))) =
-    showParens (p > 2) $ prettyPrec 2 le ++ " / " ++ prettyPrec 3 re
-prettyPrec p (Mul (Const (-1)) e) =
-    showParens (p > 3) $ "-" ++ prettyPrec 2 e
-prettyPrec p (Mul le re) =
-    showParens (p > 2) $ prettyPrec 2 le ++ " * " ++ prettyPrec 2 re
+    showParens (p > 1) $ prettyPrec 1 le ++ renderTail re
+  where
+    renderTail (Add l r) = render l ++ renderTail r
+    renderTail e = render e
+    render t
+        | Just t' <- stripNeg t = " - " ++ prettyPrec 2 t'
+        | otherwise = " + " ++ prettyPrec 1 t
+-- Unary Minus
+prettyPrec p e | Just e' <- stripNeg e = showParens (p > 3) $ "-" ++ prettyPrec 2 e'
+-- Mul
+prettyPrec p (Mul (Const (-1)) e) = showParens (p > 3) $ "-" ++ prettyPrec 2 e
+prettyPrec p (Mul le re) = showParens (p > 2) $ prettyPrec 2 le ++ "*" ++ prettyPrec 2 re
 -- Pow
-prettyPrec p (Pow le re) =
-    showParens (p > 4) $ prettyPrec 5 le ++ " ^ " ++ prettyPrec 4 re
+prettyPrec p (Pow le re) = showParens (p > 4) $ prettyPrec 5 le ++ "^" ++ prettyPrec 4 re
+-- Leaf
+prettyPrec _ (Const c) = show c
+prettyPrec _ (Var v) = v
