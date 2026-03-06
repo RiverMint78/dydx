974000720693789b9e4145e6647f2cae98cbc5b9
RiverMint78 <67481978@qq.com>
2026-03-06 16:42:18 +0800

    fix tests

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-import Dydx.Expr
 import Dydx.Diff
-import Dydx.Simplify
+import Dydx.Expr
+import Dydx.HighorderSimplify
 import Dydx.Pretty
 
 main :: IO ()
 main = do
-    let x = Var "x" :: Expr Integer 
-    let expr = Mul x (Sin (Exp x))
-    let d1 = simplify (diff "x" expr)
-    let d2 = simplify (diff "x" d1)
-    
+    let x = Var "x" :: Expr Integer
+    let expr = Pow (Pow 2 (x * 2)) $ -1
+    let d1 = simplifyFixed (diff "x" expr)
+    let d2 = simplifyFixed (diffN 99 "x" d1)
+
     putStrLn $ "f   x: " ++ pretty expr
     putStrLn $ "f'  x: " ++ pretty d1
     putStrLn $ "f'' x: " ++ pretty d2
--- test/Main.hs
+++ test/Main.hs
@@ -21,26 +21,65 @@ runNaNTests = do
     let x = Var "x" :: Expr Int
 
     -- Propagation
-    assertEq "NaN + x = NaN" NaN (simplify (Add NaN x))
-    assertEq "5 * NaN = NaN" NaN (simplify (Mul (Const 5 :: Expr Int) NaN))
-    assertEq "sin(NaN) = NaN" NaN (simplify (Sin NaN :: Expr Int))
-    assertEq "exp(NaN) = NaN" NaN (simplify (Exp NaN :: Expr Int))
+    assertEq "NaN + x = NaN" NaN (simplifyFixed (Add NaN x))
+    assertEq "5*NaN = NaN" NaN (simplifyFixed (Mul (Const 5 :: Expr Int) NaN))
+    assertEq "sin(NaN) = NaN" NaN (simplifyFixed (Sin NaN :: Expr Int))
+    assertEq "exp(NaN) = NaN" NaN (simplifyFixed (Exp NaN :: Expr Int))
 
     -- / 0
-    assertEq "1 / 0 = NaN" NaN (simplify (divide (Const 1 :: Expr Int) (Const 0)))
-    assertEq "0 ^ 0 = NaN" NaN (simplify (Pow (Const 0 :: Expr Int) (Const 0)))
-    assertEq "0 ^ -2 = NaN" NaN (simplify (Pow (Const 0 :: Expr Int) (Const (-2))))
+    assertEq "0^(-1) = NaN" NaN (simplifyFixed (divide (Const 1 :: Expr Int) (Const 0)))
+    assertEq "0^0 = NaN" NaN (simplifyFixed (Pow (Const 0 :: Expr Int) (Const 0)))
+    assertEq "0^(-2) = NaN" NaN (simplifyFixed (Pow (Const 0 :: Expr Int) (Const (-2))))
 
     -- Log NaN
-    assertEq "ln(0) = NaN" NaN (simplify (Log (Const 0 :: Expr Int)))
-    assertEq "ln(-5) = NaN" NaN (simplify (Log (Const (-5) :: Expr Int)))
+    assertEq "ln(0) = NaN" NaN (simplifyFixed (Log (Const 0 :: Expr Int)))
+    assertEq "ln(-5) = NaN" NaN (simplifyFixed (Log (Const (-5) :: Expr Int)))
 
     -- sin(ln(0)) -> sin(NaN) -> NaN
-    assertEq "sin(ln(0)) = NaN" NaN (simplify (Sin (Log (Const 0) :: Expr Int)))
+    assertEq "sin(ln(0)) = NaN" NaN (simplifyFixed (Sin (Log (Const 0) :: Expr Int)))
 
     -- diff
-    assertEq "diff(ln(0)) = NaN" NaN (simplify (diff "x" (Log (Const 0) :: Expr Int)))
-    assertEq "diff(1/0) = NaN" NaN (simplify (diff "x" (divide (Const 1) (Const 0) :: Expr Int)))
+    assertEq "diff(ln(0)) = NaN" NaN (simplifyFixed (diff "x" (Log (Const 0) :: Expr Int)))
+    assertEq "diff(1/0) = NaN" NaN (simplifyFixed (diff "x" (divide (Const 1) (Const 0) :: Expr Int)))
+
+runMoreMathTests :: IO ()
+runMoreMathTests = do
+    putStrLn "\nRunning More Math (Sqrt, Inverse Trig, Hyperbolic) tests..."
+
+    let x = Var "x" :: Expr Integer
+
+    -- d/dx[sqrt(x)] = 1 * (2 * sqrt(x))^(-1) -> 化简后应去掉 1 *
+    let expectedSqrtDiff = Pow (Mul (Const 2) (Sqrt x)) (Const (-1))
+    assertEq "simplifyFixed diff(sqrt(x))" expectedSqrtDiff (simplifyFixed (diff "x" (Sqrt x)))
+
+    -- d/dx[sinh(x)] = cosh(x) * 1 -> 化简后为 cosh(x)
+    assertEq "simplifyFixed diff(sinh(x))" (Cosh x) (simplifyFixed (diff "x" (Sinh x)))
+    -- d/dx[cosh(x)] = sinh(x) * 1 -> 化简后为 sinh(x)
+    assertEq "simplifyFixed diff(cosh(x))" (Sinh x) (simplifyFixed (diff "x" (Cosh x)))
+
+    -- d/dx[asin(x)] = 1 * sqrt(1 - x^2)^(-1) -> 化简去掉 1 *
+    let expectedAsinDiff = Pow (Sqrt (sub (Const 1) (Pow x (Const 2)))) (Const (-1))
+    assertEq "simplifyFixed diff(asin(x))" expectedAsinDiff (simplifyFixed (diff "x" (Asin x)))
+
+    -- d/dx[atan(x)] = 1 * (1 + x^2)^(-1)
+    let expectedAtanDiff = Pow (Add (Const 1) (Pow x (Const 2))) (Const (-1))
+    assertEq "simplifyFixed diff(atan(x))" expectedAtanDiff (simplifyFixed (diff "x" (Atan x)))
+
+    -- d/dx[asinh(x)] = 1 * sqrt(x^2 + 1)^(-1)
+    let expectedAsinhDiff = Pow (Sqrt (Add (Const 1) (Pow (Var "x") (Const 2)))) (Const (-1))
+    assertEq "simplifyFixed diff(asinh(x))" expectedAsinhDiff (simplifyFixed (diff "x" (Asinh x)))
+
+    -- const
+    assertEq "simplifyFixed sinh(0) = 0" (Const 0) (simplifyFixed (Sinh (Const 0 :: Expr Int)))
+    assertEq "simplifyFixed cosh(0) = 1" (Const 1) (simplifyFixed (Cosh (Const 0 :: Expr Int)))
+    assertEq "simplifyFixed sqrt(x^2) = x" x (simplifyFixed (Sqrt (Pow x (Const 2))))
+
+    -- print
+    putStrLn $ "\nSqrt:       " ++ pretty (Sqrt x)
+    putStrLn $ "Asin:       " ++ pretty (Asin x)
+    putStrLn $ "Cosh:       " ++ pretty (Cosh x)
+    putStrLn $ "Asin Diff:  " ++ (pretty . simplifyFixed . diff "x") (Asin x)
+    putStrLn $ "Atan Diff3: " ++ pretty (diffN 3 "x" (Atan x))
 
 main :: IO ()
 main = do
@@ -53,7 +92,7 @@ main = do
 
     -- Add/Sub
     assertEq "Add derivative" (Add (Const 1) (Const 0)) (diff "x" (Add x (Const 5)))
-    assertEq "Sub derivative (simplified)" (Const 1 :: Expr Integer) (simplify $ diff "x" (sub (Var "x") (Const 5)))
+    assertEq "Sub derivative (simplified)" (Const 1 :: Expr Integer) (simplifyFixed $ diff "x" (sub (Var "x") (Const 5)))
 
     -- Mul
     let mulExpr = Mul x x
@@ -61,43 +100,44 @@ main = do
     assertEq "Mul derivative" expectedMul (diff "x" mulExpr)
 
     -- Power Rule (x^2)
-    let x2 = Pow x (Const 2)
-    let expectedX2Diff = Mul (Pow x (Const 2)) (Add (Mul (Mul (Const 2) (Const 1)) (Pow x (Const (-1)))) (Mul (Const 0) (Log x)))
-    assertEq "Pow derivative (Raw x^2)" expectedX2Diff (diff "x" x2)
+    assertEq "Pow derivative (x^2) simplified" (Mul 2 x) (simplifyFixed $ diff "x" (Pow x 2))
 
-    putStrLn "\nRunning Simplify tests..."
-    assertEq "Simplify x - x" 0 (simplify (x - x)) -- instance of Num
-    assertEq "Simplify 0 + x" x (simplify (0 + x))
-    assertEq "Simplify x * 1" x (simplify (Mul x (Const 1)))
-    assertEq "Simplify -(-x)" x (simplify (neg (neg x)))
-    assertEq "Simplify x^1" x (simplify (Pow x (Const 1)))
-    assertEq "Simplify x^0" (Const 1) (simplify (Pow x (Const 0)))
+    putStrLn "\nRunning simplifyFixed tests..."
+    assertEq "simplifyFixed x - x" 0 (simplifyFixed (x - x)) -- instance of Num
+    assertEq "simplifyFixed 0 + x" x (simplifyFixed (0 + x))
+    assertEq "simplifyFixed x * 1" x (simplifyFixed (Mul x (Const 1)))
+    assertEq "simplifyFixed -(-x)" x (simplifyFixed (neg (neg x)))
+    assertEq "simplifyFixed x^1" x (simplifyFixed (Pow x (Const 1)))
+    assertEq "simplifyFixed x^0" (Const 1) (simplifyFixed (Pow x (Const 0)))
 
-    putStrLn "\nRunning Integration (Diff + Simplify) tests..."
+    putStrLn "\nRunning Integration (Diff + simplifyFixed) tests..."
 
-    assertEq "Simplify diff(x^2)" (Mul (Const 2) x) (simplify (diff "x" (Pow x (Const 2))))
-    assertEq "Simplify diff(x^3)" (Mul (Const 3) (Pow x (Const 2))) (simplify (diff "x" (Pow x (Const 3))))
-    assertEq "Simplify diff(5x)" (Const 5) (simplify (diff "x" x * 5))
-    assertEq "Simplify diff(x/5)" (Pow (Const 5) (Const (-1))) (simplify (diff "x" (divide x (Const 5))))
+    assertEq "simplifyFixed diff(x^2)" (Mul (Const 2) x) (simplifyFixed (diff "x" (Pow x (Const 2))))
+    assertEq "simplifyFixed diff(x^3)" (Mul (Const 3) (Pow x (Const 2))) (simplifyFixed (diff "x" (Pow x (Const 3))))
+    assertEq "simplifyFixed diff(5x)" (Const 5) (simplifyFixed (diff "x" x * 5))
+    assertEq "simplifyFixed diff(x/5)" (Pow (Const 5) (Const (-1))) (simplifyFixed (diff "x" (divide x (Const 5))))
 
     putStrLn "\nRunning Pretty Printer (Sugar Recovery) tests..."
 
     assertEq "Pretty Neg" "-x" (pretty (neg x))
     assertEq "Pretty Sub" "x - 5" (pretty (sub x (Const 5)))
-    assertEq "Pretty Div" "x / 5" (pretty (divide x (Const 5)))
-    assertEq "Pretty Pow" "x ^ 2" (pretty (Pow x (Const 2)))
+    assertEq "Pretty Div" "x*5^(-1)" (pretty (divide x (Const 5)))
+    assertEq "Pretty Pow" "x^2" (pretty (Pow x (Const 2)))
 
     let complexExpr = Mul x (Sin x)
-    putStrLn $ "Function:   " ++ pretty complexExpr
-    putStrLn $ "Order 1:    " ++ (pretty . simplify . diff "x") complexExpr
-    putStrLn $ "Order 2:    " ++ (pretty . simplify . diff "x" . diff "x") complexExpr
-    putStrLn $ "Order 3:    " ++ (pretty . simplify . diff "x" . diff "x" . diff "x") complexExpr
+    putStrLn $ "\nFunction:   " ++ pretty complexExpr
+    putStrLn $ "Order 1:    " ++ (pretty . simplifyFixed . diff "x") complexExpr
+    putStrLn $ "Order 2:    " ++ pretty (diffN 2 "x" complexExpr)
+    putStrLn $ "Order 99:   " ++ pretty (simplifyFixed (diffN 99 "x" complexExpr))
 
     let invX = divide (Const 1) x
-    putStrLn $ "Function:   " ++ pretty invX
+    putStrLn $ "Function:   " ++ (pretty . simplify) invX
     putStrLn $ "Derivative: " ++ (pretty . simplify . diff "x") invX
 
     -- NaN Tests
     runNaNTests
+
+    -- Math Tests
+    runMoreMathTests
 
     putStrLn "\nAll tests passed successfully!"
