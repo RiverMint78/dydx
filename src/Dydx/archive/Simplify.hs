ec4b69ca6eec65074efee0fb0506735447c0e47b
RiverMint78 <67481978@qq.com>
2026-03-06 14:35:26 +0800

    save old simplify

---
+++ src/Dydx/archive/Simplify.hs
@@ -0,0 +1,136 @@
+module Dydx.Simplify (simplify) where
+
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import Dydx.Expr
+
+simplify :: (Eq a, Ord a, Num a) => Expr a -> Expr a
+simplify NaN = NaN
+simplify (Add le re) = simplifyAdd (simplify le) (simplify re)
+simplify (Mul le re) = simplifyMul (simplify le) (simplify re)
+simplify (Pow le re) = simplifyPow (simplify le) (simplify re)
+simplify (Log e) = simplifyLog (simplify e)
+simplify (Sin e) = simplifySin (simplify e)
+simplify (Cos e) = simplifyCos (simplify e)
+simplify (Exp e) = simplifyExp (simplify e)
+simplify e = e -- Const / Var
+
+-- Add helpers
+baseAdd :: (Num a) => Expr a -> Expr a
+baseAdd (Mul (Const _) e) = e
+baseAdd (Const _) = Const 1
+baseAdd e = e
+
+flattenAdd :: Expr a -> [Expr a]
+flattenAdd (Add le re) = flattenAdd le ++ flattenAdd re
+flattenAdd e = [e]
+
+extractAdd :: (Num a) => Expr a -> (a, Expr a)
+extractAdd (Mul (Const c) e) = (c, e)
+extractAdd e = (1, e)
+
+mergeAddTerms :: (Eq a, Ord a, Num a) => [Expr a] -> [Expr a]
+mergeAddTerms [] = []
+mergeAddTerms [x] = [x]
+-- Const folding
+mergeAddTerms (Const a : Const b : xs) = mergeAddTerms (Const (a + b) : xs)
+-- Combine neighbor terms
+mergeAddTerms (x : y : xs)
+    | bx == by = mergeAddTerms (simplifyMul (Const (cx + cy)) bx : xs)
+    | otherwise = x : mergeAddTerms (y : xs)
+  where
+    (cx, bx) = extractAdd x
+    (cy, by) = extractAdd y
+
+buildAdd :: (Eq a, Num a) => [Expr a] -> Expr a
+buildAdd [] = Const 0
+buildAdd [x] = x
+buildAdd (x : xs) = Add x (buildAdd xs)
+
+simplifyAdd :: (Eq a, Ord a, Num a) => Expr a -> Expr a -> Expr a
+simplifyAdd NaN _ = NaN
+simplifyAdd _ NaN = NaN
+simplifyAdd le re = buildAdd merged
+  where
+    sorted = sortBy (comparing baseAdd) (flattenAdd le ++ flattenAdd re)
+    merged = filter (/= Const 0) (mergeAddTerms sorted)
+
+-- Mul helpers
+baseMul :: (Num a) => Expr a -> Expr a
+baseMul (Pow b _) = b
+baseMul (Const _) = Const 1
+baseMul e = e
+
+flattenMul :: Expr a -> [Expr a]
+flattenMul (Mul le re) = flattenMul le ++ flattenMul re
+flattenMul e = [e]
+
+extractMul :: (Num a) => Expr a -> (Expr a, Expr a)
+extractMul (Pow b e) = (b, e)
+extractMul e = (e, Const 1)
+
+mergeMulTerms :: (Eq a, Ord a, Num a) => [Expr a] -> [Expr a]
+mergeMulTerms [] = []
+mergeMulTerms [x] = [x]
+-- Const folding
+mergeMulTerms (Const a : Const b : xs) = mergeMulTerms (Const (a * b) : xs)
+-- Pow combine
+mergeMulTerms (x : y : xs)
+    | bx == by = mergeMulTerms (simplifyPow bx (simplifyAdd ex ey) : xs)
+    | otherwise = x : mergeMulTerms (y : xs)
+  where
+    (bx, ex) = extractMul x
+    (by, ey) = extractMul y
+
+buildMul :: (Eq a, Num a) => [Expr a] -> Expr a
+buildMul [] = Const 1
+buildMul [x] = x
+buildMul (x : xs) = Mul x (buildMul xs)
+
+simplifyMul :: (Eq a, Ord a, Num a) => Expr a -> Expr a -> Expr a
+simplifyMul NaN _ = NaN
+simplifyMul _ NaN = NaN
+simplifyMul le re
+    | Const 0 `elem` allTerms = Const 0
+    | otherwise = buildMul merged
+  where
+    allTerms = flattenMul le ++ flattenMul re
+    sorted = sortBy (comparing baseMul) (flattenMul le ++ flattenMul re)
+    merged = filter (/= Const 1) (mergeMulTerms sorted)
+
+simplifyPow :: (Eq a, Ord a, Num a) => Expr a -> Expr a -> Expr a
+simplifyPow NaN _ = NaN
+simplifyPow _ NaN = NaN
+-- Const folding
+simplifyPow (Const 0) (Const n) | n < 0 = NaN
+simplifyPow (Const 0) (Const 0) = NaN
+simplifyPow _ (Const 0) = Const 1
+simplifyPow e (Const 1) = e
+simplifyPow (Const 0) _ = Const 0
+simplifyPow (Const 1) _ = Const 1
+-- Pow combine
+simplifyPow (Pow u a) b = simplifyPow u (simplifyMul a b)
+simplifyPow le re = Pow le re
+
+simplifyLog :: (Eq a, Ord a, Num a) => Expr a -> Expr a
+simplifyLog NaN = NaN
+simplifyLog (Const n) | n <= 0 = NaN
+simplifyLog (Const 1) = Const 0
+simplifyLog (Exp e) = e
+simplifyLog e = Log e
+
+simplifySin :: (Eq a, Num a) => Expr a -> Expr a
+simplifySin NaN = NaN
+simplifySin (Const 0) = Const 0
+simplifySin e = Sin e
+
+simplifyCos :: (Eq a, Num a) => Expr a -> Expr a
+simplifyCos NaN = NaN
+simplifyCos (Const 0) = Const 1
+simplifyCos e = Cos e
+
+simplifyExp :: (Eq a, Num a) => Expr a -> Expr a
+simplifyExp NaN = NaN
+simplifyExp (Const 0) = Const 1
+simplifyExp (Log e) = e
+simplifyExp e = Exp e
