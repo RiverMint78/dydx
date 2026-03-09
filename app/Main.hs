8efe5837cd50d0559e428af2c6dfe1540adda71d
RiverMint78 <67481978@qq.com>
2026-03-09 12:22:49 +0800

    cli tool 0.1.0.0

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,51 @@
 module Main where
 
-import Dydx.Diff
+import System.IO (hFlush, stdout)
+import Text.Read (readMaybe)
+
+import Dydx.Diff (diff, diffN)
 import Dydx.Expr
-import Dydx.Simplify
-import Dydx.Pretty
+import Dydx.Parser (parseExpr)
+import Dydx.Pretty (pretty)
+import Dydx.Simplify (simplifyFixed)
 
 main :: IO ()
 main = do
-    let x = Var "x" :: Expr Integer
-    let expr = Pow (Pow 2 (x * 2)) $ -1
-    let d1 = simplifyFixed (diff "x" expr)
-    let d2 = simplifyFixed (diffN 99 "x" d1)
+    putStrLn "\nDydx available commands:"
+    putStrLn "  diff <var> <expr>      - Compute 1st derivative"
+    putStrLn "  diffN <n> <var> <expr> - Compute N-th derivative"
+    putStrLn "  simp <expr>            - Simplify expression"
+    putStrLn "  :q, :quit or exit      - Quit the program"
+    putStrLn "-----------------------------------------"
+    repl
 
-    putStrLn $ "f   x: " ++ pretty expr
-    putStrLn $ "f'  x: " ++ pretty d1
-    putStrLn $ "f'' x: " ++ pretty d2
+repl :: IO ()
+repl = do
+    putStr "dydx> "
+    hFlush stdout
+    input <- getLine
+    case words input of
+        [] -> repl
+        [cmd] | cmd `elem` [":q", ":quit", "exit"] -> putStrLn "Bye!"
+        ("diff" : var : rest) -> do
+            eval (unwords rest) (simplifyFixed . diff var)
+            repl
+        ("diffN" : nStr : var : rest) -> do
+            maybe
+                (putStrLn "Error: N must be an integer.")
+                (\n -> eval (unwords rest) (diffN n var))
+                (readMaybe nStr)
+            repl
+        ("simp" : rest) -> do
+            eval (unwords rest) simplifyFixed
+            repl
+        _ -> do
+            eval input simplifyFixed
+            repl
+
+eval :: String -> (Expr Integer -> Expr Integer) -> IO ()
+eval exprStr processFn =
+    either
+        (\err -> putStrLn $ "Parse error: " ++ err)
+        (putStrLn . pretty . processFn)
+        (parseExpr exprStr)
